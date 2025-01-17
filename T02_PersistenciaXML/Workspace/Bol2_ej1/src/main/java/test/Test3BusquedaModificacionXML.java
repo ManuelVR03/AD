181688762cc1eae57633c9be12574d053de1777c
ManuelVR03 <manuel.verdejo.ramirez.alu@iesjulioverne.es>
2025-01-17 11:08:48 +0100

    Agregar funcionalidad para mostrar todos los estudiantes y añadir una etiqueta de beca en el archivo XML; crear archivo estudiantesmodificado.xml

--- T02_PersistenciaXML/Workspace/Bol2_ej1/src/main/java/test/Test3BusquedaModificacionXML.java
+++ T02_PersistenciaXML/Workspace/Bol2_ej1/src/main/java/test/Test3BusquedaModificacionXML.java
@@ -3,10 +3,13 @@ package test;
 import java.io.IOException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -39,9 +42,31 @@ public class Test3BusquedaModificacionXML {
 			// Muestra los elementos del estudiante 493
 			System.out.println("Xpath muestra los datos del estudiante 493");
 			Node nodoClase2 = (Node)(XPathFactory.newInstance().newXPath().evaluate("//estudiante[@rollno='493']", documento.getDocumento(), XPathConstants.NODE));
-			if(nodoClase != null) {
+			if(nodoClase2 instanceof Element) {
 				System.out.println(nodoClase2.getTextContent());
 			}
+			
+			// Muestra todos los estudiantes
+			System.out.println("Xpath muestra ...");
+			XPath xpath = XPathFactory.newInstance().newXPath();
+			String xPathExpression = "/clase/estudiante";
+			NodeList nodos = (NodeList) xpath.evaluate(xPathExpression, documento.getDocumento(), XPathConstants.NODESET);
+			for (int i=0;i<nodos.getLength();i++) {
+				System.out.println(nodos.item(i).getNodeName() + " : " + nodos.item(i).getTextContent());
+			}
+			System.out.println();
+			
+			// Añade una nueva etiqueta llamada beca a ese estudiante.
+			// Crea estudiantesmodificado.xml con esta nueva información
+			// Muestralo en pantalla
+			System.out.println("******************************************************************************");
+			Document nuevoDocumento = documento.getDocumento();
+			Element beca = nuevoDocumento.createElement("Beca");
+			beca.setTextContent("Si");
+			nodoClase2.appendChild(beca);
+			documento.escribirArchivo("./Salidas/estudiantesmodificado.xml");
+			DomXML nuevoDocumentoLectura = new DomXML("./Salidas/estudiantesmodificado.xml");
+			System.out.println(nuevoDocumentoLectura.convertirString());
 			
 		}catch(ParserConfigurationException e) {
 			e.printStackTrace();
@@ -49,6 +74,9 @@ public class Test3BusquedaModificacionXML {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
